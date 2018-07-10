62837704caf28e8c7559b3e0a41d05a3960ba70d
samael33 <IgorOlhovskiy@gmail.com>
2018-07-10 19:20:20 +0300

    ...

--- migrate/remove_4.2_dialplans.php
+++ migrate/remove_4.2_dialplans.php
@@ -1,24 +1,24 @@
 <?php
 
-$execute_sql = true;
-$document_root = '/var/www/fusionpbx';
+$execute_sql = false;
+$config_path = '/etc/fusionpbx/';
 
 $dialplans_to_delete = ['user_exists', 'user_record', 'call_forward_all', 'local_extension'];
 
-//web server or command line
-if(defined('STDIN')) {
-        set_include_path($document_root);
-        $_SERVER["DOCUMENT_ROOT"] = $document_root;
-        $project_path = $_SERVER["DOCUMENT_ROOT"];
-        define('PROJECT_PATH', $project_path);
-        $_SERVER["PROJECT_ROOT"] = realpath($_SERVER["DOCUMENT_ROOT"] . PROJECT_PATH);
-        set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER["PROJECT_ROOT"]);
-        require_once "resources/require.php";
-        $display_type = 'text'; //html, text
-} else {
-    include "root.php";
-    require_once "resources/require.php";
-    require_once "resources/pdo.php";
+require_once $config_path . "config.php";
+
+try {
+    if (strlen($db_host) > 0) {
+        if (strlen($db_port) == 0) { $db_port = "5432"; }
+        $db = new PDO("pgsql:host=$db_host port=$db_port dbname=$db_name user=$db_username password=$db_password");
+    }
+    else {
+        $db = new PDO("pgsql:dbname=$db_name user=$db_username password=$db_password");
+    }
+}
+catch (PDOException $error) {
+    print "error: " . $error->getMessage() . "<br/>";
+    die();
 }
 
 foreach ($dialplans_to_delete as $dialplan_name) {
